29768fd2cc899eed4598f9924e5b0b6c3fdf400d
Artyom <artvirsh@mail.ru>
2018-09-11 18:38:37 +0300

    Edit Decision edited

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 module.exports = function solveEquation(equation) {
   let x1 = 0, x2 = 0, d = 0; 
 
-  equation = equation.replace(/\s/g,'').match(/(-|\+|[0-9])[0-9]*/g);
+equation = equation.replace(/\s/g,'').match(/(-|\+|[0-9])[0-9]*/g);
+
   equation.splice(1, 1);
   
   let [a, b, c] = equation;
   
-  d = (+b)*(+b) - 4*(+a)*(+c);
+  d = Math.pow(b, 2) - 4 * a * c;
   
-  x1 = Math.round((-1*b + Math.sqrt(d)) / (2 * a));
+  x1 = Math.round((-1 * b + Math.sqrt(d)) / (2 * a));
   x2 = Math.round(-1*((+b) + Math.sqrt(d)) / (2 * a));
    
   return [x1, x2].sort((a,b)=>a-b);
